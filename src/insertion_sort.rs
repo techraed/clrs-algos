ea9a280f80c3c09ae1709bb042243871d81bf3a3
Sabaun Taraki <taraki.sabaun.rl@gmail.com>
2021-02-15 22:07:30 +0300

    clrs insertion sort simpyfied

--- src/insertion_sort.rs
+++ src/insertion_sort.rs
@@ -16,10 +16,9 @@ fn insertion_sort_1<T: PartialOrd + Clone>(src: &mut [T]) {
     }
 }
 
-// Look throughly at `insertion_sort_2` and `insertion_sort_3`. They show the same sorting idea: you
-// put `current` value in the proper place only once. To do that you have to free the place for it.
-// So you move to the right all the values in range of `0..i`, which are bigger than `src[i]` (i.e.
-// `current`).
+// Look throughly at `insertion_sort_3`. You put `current` value in the proper place only once. 
+// To do that you have to free the place for it. So you move to the right all the values in range of `0..i`, 
+// which are bigger than `src[i]` (i.e.`current`).
 //
 // But these implementations have crucial things to mention. Index `j` points to numbers that
 // potentially could be moved to the right if the condition is met (so `src[j+1] = src[j]`).
@@ -27,49 +26,33 @@ fn insertion_sort_1<T: PartialOrd + Clone>(src: &mut [T]) {
 // That's how values are moved to the right freeing place for `current`, which is smaller then
 // them.
 // However, if you reached the beginning (i.e. `j == 0` and `src[j] > current`), it means
-// you found the smallest (currenty) value of the 0..i range.
-// Logically, it's place is src[0] (`src[j]`), not src[1] (so `src[j+1]`).
+// you found the smallest (currently) value of the 0..i range.
+// Logically, it's place is src[0] (`src[j]`), not src[1] (`src[j+1]`).
 //
-// We could avoid all that stuff if `j` was `isize`, but it's quite strange to do conversion from
+// We could avoid all that stuff under while loop if `j` was `isize`, but it's quite strange to do conversion from
 // `isize` to `usize` all the time.
-
-fn insertion_sort_2<T: PartialOrd + Clone>(src: &mut [T]) {
-    for i in 1..src.len() {
-        let current = src.get(i).expect("out of bounds").clone();
-        let mut j = i - 1;
-        while Some(&current) < src.get(j) {
-            src[j + 1] = src[j].clone();
-            if j == 0 {
-                break;
-            }
-            j -= 1;
-        }
-        let j = if j == 0 && &src[j] == &src[j + 1] { j } else { j + 1 };
-        src[j] = current;
-    }
-}
-
-fn insertion_sort_3<T: PartialOrd + Copy>(src: &mut [T]) {
+fn insertion_sort_2<T: PartialOrd + Copy>(src: &mut [T]) {
     for i in 1..src.len() {
         let current = src[i];
         let mut j = i - 1;
-        loop {
-            if current >= src[j] {
-                j += 1;
-                break;
-            }
+        while j > 0 && src[j] > current {
             src[j + 1] = src[j];
-            if j == 0 {
-                break;
-            }
             j -= 1;
         }
-        src[j] = current;
+
+        if src[j] > current {
+            // if we entered the path, then j == 0
+            // so we found currently smallest value of the 0..i range.
+            src[j + 1] = src[j];
+            src[j] = current;
+        } else {
+            src[j + 1] = current;
+        }
     }
 }
 
 // Same as insertion_sort_1, but more readable.
-fn insertion_sort_4<T: PartialOrd + Clone>(src: &mut [T]) {
+fn insertion_sort_3<T: PartialOrd + Clone>(src: &mut [T]) {
     for cur in 1..src.len() {
         let mut i = cur;
         while i > 0 && src[i] < src[i - 1] {
@@ -84,16 +67,13 @@ fn insertion_sort_test() {
     for (input, sorted) in get_sort_tests().iter_mut() {
         let mut input2 = input.clone();
         let mut input3 = input.clone();
-        let mut input4 = input.clone();
 
         insertion_sort_1(input);
         insertion_sort_2(&mut input2);
         insertion_sort_3(&mut input3);
-        insertion_sort_4(&mut input4);
 
         assert_eq!(input, sorted);
         assert_eq!(&mut input2, sorted);
         assert_eq!(&mut input3, sorted);
-        assert_eq!(&mut input4, sorted);
     }
 }
