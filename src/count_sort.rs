68f9a6426e51f4c3f8d945c291150c995c930fc3
Sabaun Taraki <taraki.sabaun.rl@gmail.com>
2021-11-06 10:54:02 +0300

    filling complmentary arrays

--- src/count_sort.rs
+++ src/count_sort.rs
@@ -14,13 +14,15 @@
 //! Should be mentioned that it is a [stable](https://en.wikipedia.org/wiki/Sorting_algorithm#Stability) sorting algorithm, which is an important feature
 //! for some other algorithms that can use count sort, for example, [radix sort](todo).
 
-use num::ToPrimitive;
+use num::{ToPrimitive, Unsigned, FromPrimitive, Signed};
+use std::fmt::Debug;
 
 /// Count sort entry function
 ///
 /// If `src` inner data has a primitive type and can be casted to usize, then the sort will be performed.
 /// Otherwise no mutations will be made on `src`.
-pub fn count_sort<T: ToPrimitive + Ord + Copy>(src: &mut [T]) {
+pub fn count_sort<T: ToPrimitive + Unsigned + Ord + Copy + Debug>(src: &mut [T]) {
+    // let min_element = src.iter().min();
     let max_element = src.iter().max();
     if max_element.map(ToPrimitive::to_usize).flatten().is_some() {
         count_sort_impl(src, max_element.copied().expect("is some value"))
@@ -31,23 +33,44 @@ pub fn count_sort<T: ToPrimitive + Ord + Copy>(src: &mut [T]) {
 ///
 /// Because of conditions stated in the module [doc]() we parametrize function with a number type value.
 /// Unfortunately this algorithm requires additional space, which is a good example of space - time tradeoff.
-fn count_sort_impl<T: ToPrimitive + PartialOrd + Copy>(src: &mut[T], max_element: T) {
+fn count_sort_impl<T: ToPrimitive + Unsigned + Ord + Copy + Debug>(src: &mut[T], max_element: T) {
     match src.len() {
         0 | 1 => {}
-        _ => count_sort_rec_proc(src, max_element),
+        _ => count_sort_proc(src, max_element),
     }
 }
 
 /// Count sort recursive procedure
-fn count_sort_rec_proc<T: ToPrimitive + PartialOrd + Copy>(src: &mut[T], max_element: T) {
-    // src
-    //     .iter()
-    //     .filter_map(usize::from)
-    //
-    //
-    // let max_element = usize::from(max_element).expect("rec proc callable for types, that can be converted to usize");
-    // let amount_of_elements = Vec::with_capacity(max_element);
-    // for k in src {
-    //
-    // }
+/// todo передай сразу usize элементы
+fn count_sort_proc<T: ToPrimitive + Unsigned + Ord + Copy + Debug>(src: &mut[T], max_element: T) {
+    println!("{:?}", src);
+    let max_element = max_element.to_usize().expect("rec proc callable for types, that can be converted to usize");
+    let mut keys_count: Vec<usize> = vec![0; max_element + 1];
+    for key in src {
+        if let Some(key) = key.to_usize() {
+            println!("{:?}", key);
+            keys_count[key] += 1;
+        }
+    }
+    let mut less_than_key_count = keys_count.clone();
+    for (idx, _) in keys_count.iter().enumerate() {
+        if idx == 0 { continue; }
+        less_than_key_count[idx] = less_than_key_count[idx] + less_than_key_count[idx - 1];
+    }
+
+    println!("KEY COUNT = {:?}", keys_count);
+    println!("KEY LESS THAN COUNT = {:?} \n", less_than_key_count);
 }
+
+#[test]
+fn count_sort_test() {
+    use num::FromPrimitive;
+
+    use crate::test_utils::get_test_vectors;
+
+    for (input, _) in get_test_vectors() {
+        let mut input = input.into_iter().flat_map(usize::from_i32).collect::<Vec<_>>();
+        count_sort(&mut input[..]);
+    }
+}
+
